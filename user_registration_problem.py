86daefd6b44afb2697a118008481e5d5c4e88888
Haripreetam <haripreetam333@gmail.com>
2024-08-07 14:07:05 +0530

    [Haripreetam] ADD:python script for validating the email id using the regex pattern

--- user_registration_problem.py
+++ user_registration_problem.py
@@ -10,3 +10,30 @@ def valid_first_name(name):
         return "Valid first name"
     else:
         return "Invalid first name"
+    
+    
+def valid_last_name(name):
+    '''Description : To validate the last name using regex pattern.
+        Parameters : name - It is a string 
+        Return : 'Valid last name' if the name is valid, else 'Invalid last name
+    '''
+    pattern = r'^[A-Z][a-zA-Z]{2,}$'
+    if re.match(pattern, name):
+        return "Valid last name"
+    else:
+        return "Invalid last name"
+    
+    #for full name ^[A-Z][a-zA-Z]{2,}\s[A-Z][a-zA-Z]{2,}$ 
+
+
+def valid_email(email):
+    '''Description : To validate the email using regex pattern.
+        Parameters : email - It is a string 
+        Return : 'Valid email' if the name is valid, else 'Invalid email'
+    '''
+    pattern = r'^([a-zA-Z0-9]{3,})+(\.[a-zA-Z0-9]+)?@[a-zA-Z]{2,}\.[a-z]{2,}(?:\.[a-zA-Z]{2,})?$'
+    #if more than two domain like ".gov.co.uk" then you can use '(){0,2}'
+    if re.match(pattern, email):
+        return "Valid email"
+    else:
+        return "Invalid email"
