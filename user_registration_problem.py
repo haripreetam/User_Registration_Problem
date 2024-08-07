ab718cdfb3855665a187208cf9a858c741efa886
Haripreetam <haripreetam333@gmail.com>
2024-08-07 10:54:22 +0530

    [Haripreetam] ADD:python script for validating the lastname  using regex pattern

--- user_registration_problem.py
+++ user_registration_problem.py
@@ -3,11 +3,22 @@ import re
 def UC01_valid_first_name(name):
     pattern = r'^[A-Z][a-zA-Z]{2,}$'
     if re.match(pattern, name):
-        return True
-    return False
+        return "Valid first name"
+    else:
+        return "Invalid first name"
+
+
+def UC02_valid_last_name(name):
+    pattern = r'^[A-Z][a-zA-Z]{2,}$'
+    if re.match(pattern, name):
+        return "Valid last name"
+    else:
+        return "Invalid last name"
+
+#for full name ^[A-Z][a-zA-Z]{2,}\s[A-Z][a-zA-Z]{2,}$ 
 
 print(UC01_valid_first_name("Hari"))
 print(UC01_valid_first_name("Ha"))  
-print(UC01_valid_first_name("hari"))
-print(UC01_valid_first_name("Hari123"))
+print(UC02_valid_last_name("reddy"))
+print(UC02_valid_last_name("Reddy"))
  
